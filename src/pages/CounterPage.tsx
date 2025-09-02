124cae03401e7c108f82108161d785569e35cbb5
Git-Andrew-Git <177481666+Git-Andrew-Git@users.noreply.github.com>
2025-09-02 14:50:08 +0200

    implement counter

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -6,7 +6,7 @@ const CounterPage = () => {
      * 
      * Plus d'information sur la déclaration des useState : https://www.carlrippon.com/typed-usestate-with-typescript/
      */
-
+    const [counter, setCounter] = useState(0);
 
     /**
      * TODO implémenter Fonction permettant d'incrémenter l'état du compteur.
@@ -15,7 +15,12 @@ const CounterPage = () => {
      * 
      * Pour un exemple d'implémentation d'un bouton vous pourrez vous inspirer de l'article suivant : https://www.kindacode.com/article/react-typescript-handling-onclick-event#example-1:-button-onclick
      */
+    const counterHandler = (event: React.MouseEvent<HTMLButtonElement>) =>{
+        event.preventDefault();
 
+
+        setCounter(prev => prev + 1);
+    };
 
     /**
      * TODO implémenter un fonction permettant d'incrémenter l'état du compteur
@@ -30,6 +35,11 @@ const CounterPage = () => {
     return (
         <>
             <h2>Compteur :</h2>
+            <button onClick={counterHandler} className="button" name = "button">Click me</button>
+            
+            <h1>
+                {counter !== 0 ? `You clicked button "${counter}" times` : "You haven't clicked button yet"}
+            </h1>
             {/* TODO compléter le code affichant le nombre de clics et des boutons */}
         </>
     )
